d95482ffcd6526faa21ecda62ecf22dfe5320dbb
enparthasarathi <Parthasarathi.G@encora.com>
2025-05-07 12:36:00 +0530

    fix: update broker settings

--- mkdirs.sh
+++ mkdirs.sh
@@ -14,18 +14,3 @@ sudo chown 1000:1000 -R .data/kafka-1
 
 mkdir -p .data/kafka-2
 sudo chown 1000:1000 -R .data/kafka-2
-
-mkdir -p .data/kafka-3
-sudo chown 1000:1000 -R .data/kafka-3
-
-mkdir -p .data/kafka-4
-sudo chown 1000:1000 -R .data/kafka-4
-
-mkdir -p .data/kafka-5
-sudo chown 1000:1000 -R .data/kafka-5
-
-mkdir -p .data/kafka-6
-sudo chown 1000:1000 -R .data/kafka-6
-
-mkdir -p .data/kafka-7
-sudo chown 1000:1000 -R .data/kafka-7
